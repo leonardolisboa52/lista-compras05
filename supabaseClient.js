020e6bec7c583c369edbdf10089f3a20bc10a1ea
leonardolisboa52 <160758246+leonardolisboa52@users.noreply.github.com>
2025-05-13 07:53:14 -0300

    leonardo

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://wqrvadycionozczmqgoj.supabase.co',
+  'https://owsxhloqlysyzxafbabl.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93c3hobG9xbHlzeXp4YWZiYWJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU3MDksImV4cCI6MjA2MjExMTcwOX0.Zqi4P4NIIJV_ZipQOUb6ly1RmT1mvclXKgyLNqviIn0'
 )
